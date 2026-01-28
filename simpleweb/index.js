e3d6e8156030ac34899fdf37fd69c4defa212305
Jesper Raundahl <zacban@gmail.com>
2026-01-28 17:02:58 +0100

    Add Dockerfile and Express server for visits service with Redis integration

--- simpleweb/index.js
+++ simpleweb/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    res.send('Hello, World! Now in usr/app directory.<br /><i>now with less cache busting<(i>');
 });
 
 app.listen(8080, () => {
